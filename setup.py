fa00cfcb0adbd1d2da53abee70439b3f9cffa2ea
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-10-08 22:09:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="",
     version="0.1.0",
-    py_modules=["my_pipeline", ],
-    install_requires=["kedro"],
+    py_modules=["bob", ],
+    install_requires=[""],
 )
